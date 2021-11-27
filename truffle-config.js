4e16b5b01368fd24f7fddcffd949cff778164247
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-27 12:38:03 +0100

    Update truffle config to work without .secrets folder in project

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,26 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secrets/mnemonic").toString().trim();
-const projectId = fs.readFileSync(".secrets/infura-project-id").toString().trim();
+
+let provider;
+if (fs.existsSync(".secrets")) {
+  const mnemonic = fs.readFileSync(".secrets/mnemonic").toString().trim();
+  const projectId = fs.readFileSync(".secrets/infura-project-id").toString().trim();
+
+  provider = () => new HDWalletProvider({
+    mnemonic: mnemonic,
+    providerOrUrl: "https://rinkeby.infura.io/v3/" + projectId,
+  });
+}
 
 module.exports = {
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
     rinkeby: {
-      provider: () => new HDWalletProvider({
-        mnemonic: mnemonic,
-        providerOrUrl: "https://rinkeby.infura.io/v3/" + projectId
-      }),
+      provider: provider,
       network_id: 4,
       gas: 5500000,
       confirmations: 2,
