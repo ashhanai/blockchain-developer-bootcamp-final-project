d52aa0fc8fd4771bd44bc6b59b03c9e0c1ebaa74
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-27 12:38:52 +0100

    Update migration script to deploy faucets

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const P2PLoan = artifacts.require("P2PLoan");
+const ERC20Faucet = artifacts.require("ERC20Faucet");
+const ERC721Faucet = artifacts.require("ERC721Faucet");
 
 module.exports = function(deployer) {
   deployer.deploy(P2PLoan);
+  deployer.deploy(ERC20Faucet);
+  deployer.deploy(ERC721Faucet);
 };
